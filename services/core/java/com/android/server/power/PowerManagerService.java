80f76b9cd0ead91bcb750aa276756d7584ecf306
Danny Baumann <dannybaumann@web.de>
2013-08-05 10:32:19 +0200

    Allow adjusting progress on touch events.
    Change-Id: Ide0a7ae057a62f3631a32eeaa63f3e04dec5bf54

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1688,7 +1688,7 @@ public final class PowerManagerService extends SystemService
                             keyboardBrightness = mKeyboardBrightness;
                         }
 
-                        mKeyboardLight.setBrightness(mKeyboardVisible ? keyboardBrightness : 0);
+                        //mKeyboardLight.setBrightness(mKeyboardVisible ? keyboardBrightness : 0);
                         if (mButtonTimeout != 0 && now > mLastUserActivityTime + mButtonTimeout) {
                              mButtonsLight.setBrightness(0);
                         } else {
@@ -1702,7 +1702,7 @@ public final class PowerManagerService extends SystemService
                         nextTimeout = mLastUserActivityTime + screenOffTimeout;
                         if (now < nextTimeout) {
                             mButtonsLight.setBrightness(0);
-                            mKeyboardLight.setBrightness(0);
+                            //mKeyboardLight.setBrightness(0);
                             mUserActivitySummary = USER_ACTIVITY_SCREEN_DIM;
                         }
                     }
