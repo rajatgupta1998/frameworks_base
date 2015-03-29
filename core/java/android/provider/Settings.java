216b611cd21e5d0b3b3d707e238e622791a6f414
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    [1/3] base: allow disable of screenshot shutter sound
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4285,6 +4285,13 @@ public final class Settings {
         public static final String POWERMENU_ADVANCED_REBOOT = "powermenu_advanced_reboot";
 
         /**
+         * Screenshod sound enable, This is the noise made when taeking a screesnhot
+         * Defaults to 1 - sounds enabled
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * Whether to display the screenshot option on the power menu
          *
          * @hide
