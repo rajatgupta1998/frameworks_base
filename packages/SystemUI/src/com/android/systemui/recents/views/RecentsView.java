d6d214152457ed1bd19cf0052cd5c170b6f45a47
Dave Kessler <activethrasher00@gmail.com>
2015-04-04 15:47:24 +0200

    Add membar to recents options [1/2]
    Change-Id: Ieaf86a2360cc993bb20f971b25c2b8c49a746f64
    
    https://github.com/BenzoRoms/frameworks_base/commit/98de755cb5cb56b1778fd7fd38a9d36e90b2ade5
    https://github.com/BenzoRoms/frameworks_base/commit/0a454d19e453b7294c6df54cbb80b217405a2798
    
    thanks to varund7726 for the spacing
    
    Change-Id: Id1e799729543e385767ec8b68b126a20c4874baf
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -18,6 +18,8 @@ package com.android.systemui.recents.views;
 
 import static android.app.ActivityManager.StackId.INVALID_STACK_ID;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.MemoryInfo;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
@@ -58,6 +60,7 @@ import android.view.ViewPropertyAnimator;
 import android.view.Window;
 import android.view.WindowInsets;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.ImageButton;
 
@@ -104,6 +107,12 @@ import com.android.systemui.stackdivider.WindowManagerProxy;
 import com.android.systemui.statusbar.FlingAnimationUtils;
 import com.android.systemui.statusbar.phone.ScrimController;
 
+import java.io.BufferedReader;
+import java.io.FileDescriptor;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -212,7 +221,8 @@ public class RecentsView extends FrameLayout {
             mStackButtonShadowColor = mStackActionButton.getShadowColor();
             addView(mStackActionButton);
         }
-
+        mSettingsObserver = new SettingsObserver(new Handler());
+        mAm = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
         reevaluateStyles();
     }
 
@@ -520,6 +530,8 @@ public class RecentsView extends FrameLayout {
             EventBus.getDefault().send(new DismissAllTaskViewsEvent());
             }
         });
+        mMemText = (TextView) ((View)getParent()).findViewById(R.id.recents_memory_text);
+        mMemBar = (ProgressBar) ((View)getParent()).findViewById(R.id.recents_memory_bar);
         super.onAttachedToWindow();
     }
 
@@ -542,11 +554,13 @@ public class RecentsView extends FrameLayout {
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final ContentResolver resolver = mContext.getContentResolver();
         int width = MeasureSpec.getSize(widthMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
 
         if (mTaskStackView.getVisibility() != GONE) {
             mTaskStackView.measure(widthMeasureSpec, heightMeasureSpec);
+        showMemDisplay();
         }
 
         // Measure the empty view to the full size of the screen
@@ -609,6 +623,42 @@ public class RecentsView extends FrameLayout {
         LayoutInflater inflater = LayoutInflater.from(mContext);
         float cornerRadius = mContext.getResources().getDimensionPixelSize(
                     R.dimen.recents_task_view_rounded_corners_radius);
+    }
+
+    private boolean showMemDisplay() {
+        boolean enableMemDisplay = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SYSTEMUI_RECENTS_MEM_DISPLAY, 0) == 1;
+
+        if (!enableMemDisplay) {
+            mMemText.setVisibility(View.GONE);
+            mMemBar.setVisibility(View.GONE);
+            return false;
+        }
+        mMemText.setVisibility(View.VISIBLE);
+        mMemBar.setVisibility(View.VISIBLE);
+
+        updateMemoryStatus();
+        return true;
+    }
+
+    private void updateMemoryStatus() {
+        if (mMemText.getVisibility() == View.GONE
+                || mMemBar.getVisibility() == View.GONE) return;
+
+        MemoryInfo memInfo = new MemoryInfo();
+        mAm.getMemoryInfo(memInfo);
+            int available = (int)(memInfo.availMem / 1048576L);
+            int max = (int)(getTotalMemory() / 1048576L);
+            mMemText.setText("Free RAM: " + String.valueOf(available) + "MB");
+            mMemBar.setMax(max);
+            mMemBar.setProgress(available);
+    }
+
+    public long getTotalMemory() {
+        MemoryInfo memInfo = new MemoryInfo();
+        mAm.getMemoryInfo(memInfo);
+        long totalMem = memInfo.totalMem;
+        return totalMem;
     }
 
     /**
@@ -898,6 +948,7 @@ public class RecentsView extends FrameLayout {
             // Animate the background away only if we are dismissing Recents to home
             animateBackgroundScrim(0f, DEFAULT_UPDATE_SCRIM_DURATION);
         }
+        updateMemoryStatus();
     }
 
     public final void onBusEvent(ShowStackActionButtonEvent event) {
