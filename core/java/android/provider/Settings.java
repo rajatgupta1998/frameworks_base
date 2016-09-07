8e2acbadfa29b995491d21b3a8c9046e8b5af066
maxwen <max.weninger@gmail.com>
2016-09-07 11:20:19 +0200

    base: add alarm fullscreen
    Change-Id: Idafc6683baff3e6919eb5d07017aeb8e9b47f6ea

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3984,6 +3984,13 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Should the alarm be shown in fullscreen instead of heads-up
+         *
+         * @hide
+         */
+        public static final String SHOW_ALARM_FULLSCREEN = "show_alarm_fullscreen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4097,6 +4104,7 @@ public final class Settings {
             PUBLIC_SETTINGS.add(HAPTIC_FEEDBACK_ENABLED);
             PUBLIC_SETTINGS.add(SHOW_WEB_SUGGESTIONS);
             PUBLIC_SETTINGS.add(VIBRATE_WHEN_RINGING);
+            PUBLIC_SETTINGS.add(SHOW_ALARM_FULLSCREEN);
         }
 
         /**
