a4cd9176407bd38723706fb437f225738aa4a507
XXMrHyde <xxmrhyde@gmx.co.uk>
2017-04-20 22:38:49 -0400

    Bring back portrait/landscape QS mod [1/2]
    This is just adding back part of the code that we had before
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/faa0970f4c82d4a6c0b2e925cb09f626b4baac86
    
    https://i.imgur.com/pXsr3AH.png
    
    https://i.imgur.com/5ked5gQ.png
    
    https://i.imgur.com/9YCVYty.png
    
    Change-Id: Ibe7c035c2a101f8a2300d7bb2426c1a1659b85ce
    
    Conflicts:
    	core/res/res/values/aquarios_symbols.xml
    	core/res/res/values/cr_dimens.xml
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -600,7 +600,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.STATUS_BAR_SHOW_TICKER),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.QS_LAYOUT_COLUMNS),
+                    Settings.System.QS_LAYOUT_COLUMNS), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_ROWS_PORTRAIT),
+                    false, this, UserHandle.USER_ALL);
+             resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_ROWS_LANDSCAPE),
                     false, this, UserHandle.USER_ALL);
             update();
         }
@@ -630,6 +636,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         Settings.System.STATUS_BAR_SHOW_TICKER,
                         0, UserHandle.USER_CURRENT) == 1;
                 initTickerView(); 
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_ROWS_PORTRAIT))
+                    || uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_ROWS_LANDSCAPE))) {
+                    updateResources();
             }
             update();
         }
