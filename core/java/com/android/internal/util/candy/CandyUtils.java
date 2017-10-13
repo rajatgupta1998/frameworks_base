95eeb2dc7141dbc1da577fe657fd7b1968eddcc3
Marko Man <darkobas@gmail.com>
2017-10-13 16:48:20 +0200

    PiP tile: add toast if pip mode is not avail
    Change-Id: Idfa864c7b26f7965afb233ae377b4a520da326a8

--- core/java/com/android/internal/util/candy/CandyUtils.java
+++ core/java/com/android/internal/util/candy/CandyUtils.java
@@ -16,8 +16,10 @@
 
 package com.android.internal.util.candy;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -46,6 +48,8 @@ import android.view.KeyEvent;
 import java.util.Locale;
 
 import com.android.internal.statusbar.IStatusBarService;
+
+import java.util.List;
 
 public class CandyUtils {
 
@@ -190,6 +194,22 @@ public class CandyUtils {
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
             }
         }, 20);
+    }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
     }
 
     private static final class FireActions {
