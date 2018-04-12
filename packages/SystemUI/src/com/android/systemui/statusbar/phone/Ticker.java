3cca31931295c6c6aa689c41227b25c4087d7ace
Dan Cartier <nospamdan1@gmail.com>
2018-04-12 08:47:24 -0400

    ticker: Fix missing code from refactoring
    Thanks @ezio84!
    Original commit: https://github.com/ezio84/abc_frameworks_base/commit/5b0a396466766cfb1fa8bfcd46cc8e2e495b5cbb
    
    Change-Id: I372a6705112265be530c02a3a6b5a1ba2ce170ac

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
@@ -87,7 +87,10 @@ public abstract class Ticker implements DarkReceiver {
         StaticLayout getLayout(CharSequence substr) {
             int w = mTextSwitcher.getWidth() - mTextSwitcher.getPaddingLeft()
                     - mTextSwitcher.getPaddingRight();
-            return new StaticLayout(substr, mPaint, w, Alignment.ALIGN_NORMAL, 1, 0, true);
+            if (w > 0) {
+                return new StaticLayout(substr, mPaint, w, Alignment.ALIGN_NORMAL, 1, 0, true);
+            }
+            return null;
         }
 
         CharSequence rtrim(CharSequence substr, int start, int end) {
@@ -107,6 +110,9 @@ public abstract class Ticker implements DarkReceiver {
             }
             CharSequence substr = this.text.subSequence(this.current, this.text.length());
             StaticLayout l = getLayout(substr);
+            if (l == null) {
+                return null;
+            }
             int lineCount = l.getLineCount();
             if (lineCount > 0) {
                 int start = l.getLineStart(0);
