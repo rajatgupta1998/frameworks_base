30a1c20c73ca9fbfe6629bbb16cca34378562eec
Alex <alex@dirtyunicorns.com>
2018-06-10 01:33:38 -0500

    Remove deadpool analog clock
    Change-Id: I426dcbe93d05e5c861495858d8c7b70efd2d8dae

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
@@ -178,7 +178,7 @@ public class KeyguardClockPositionAlgorithm {
     }
 
     public void setClockSelection(int style) {
-        mIsBigClock = style == 3;
+        mIsBigClock = style > 1;
     }
 
     private float burnInPreventionOffsetY() {
